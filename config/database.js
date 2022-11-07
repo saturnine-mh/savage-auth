019269cec9289488cfe19a606b5402d779e9bcfd
saturnine.mh <zainnasir899@icloud.com>
2022-11-06 22:55:14 -0500

    cmmit

---
+++ config/database.js
@@ -0,0 +1,6 @@
+// config/database.js
+module.exports = {
+
+    'url' : 'mongodb+srv://zain:Football899@cluster0.a7z8gpc.mongodb.net/savage-auth?retryWrites=true&w=majority', 
+    'dbName': 'savage-auth'
+};
